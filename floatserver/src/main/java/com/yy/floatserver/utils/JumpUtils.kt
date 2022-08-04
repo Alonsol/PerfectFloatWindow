a97e35f7f29954c6675012284f4f71ccb3a3eda0
Tianze Ds Qiu <slaaker@126.com>
2022-08-05 07:29:30 +0800

    try fix for api 31
    to avoid an issue on android 12

--- floatserver/src/main/java/com/yy/floatserver/utils/JumpUtils.kt
+++ floatserver/src/main/java/com/yy/floatserver/utils/JumpUtils.kt
@@ -3,6 +3,7 @@ package com.yy.floatserver.utils
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 /**
  * Created by yy on 2020/6/13.
@@ -11,10 +12,14 @@ import android.content.Intent
 object JumpUtils {
 
     fun jump(context: Context, clazz: Class<*>?) {
-        clazz?:return
+        clazz ?: return
         var intent = Intent(context, clazz)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        var pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+        var pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(context, 0, intent, 0)
+        }
         try {
             pendingIntent.send()
         } catch (e: Exception) {
